e66e938d9eaa502adcc654343712c2bf019e4682
Logan Sinclair <rocket3701@yahoo.com>
2017-08-24 06:47:36 -0500

    Created Client Side index.html
    and added some console.logs to app.js

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var express = require("express"),
 	server 	= require("http").Server(app),
 	io 		= require("socket.io")(server);
 
-server.listen(80);
+server.listen(3000, function(){
+	console.log("The Server has started!")
+});
 
 app.get("/", function(req, res){
-	res.sendfile(_dirname + "/index.html");
+	res.sendFile(__dirname + "/index.html");
 })
 
 io.on("connection", function(socket){
+	console.log("Someone has connected!");
 	socket.emit("news", {hello: "world"});
 	socket.on("my other event", function(data){
 		console.log("data");
