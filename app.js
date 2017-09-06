191c684c7bdd6d068154f9c38cb87d35243af6f2
Logan Sinclair <rocket3701@yahoo.com>
2017-09-05 22:07:04 -0500

    players now work with socket.io (bullets broke)

--- app.js
+++ app.js
@@ -11,10 +11,32 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");
 })
 
+var characters = [];
+var bullets = [];
+
 io.on("connection", function(socket){
+	socket.emit("connection", {successfully: "connected!"});
 	console.log("Someone has connected!");
-	socket.emit("news", {hello: "world"});
-	socket.on("my other event", function(data){
-		console.log("data");
+	socket.on("character", function(data){
+		for(var i = 0; i < characters.length; i++){
+			if(characters[i].character.id === data.character.id){
+				characters.splice(i, 1);
+			}
+		}
+		console.log("Player Count: " + characters.length);
+		characters.push(data);
+		socket.emit("serverCharacters", {data: characters});
+	})
+	socket.on("newBullet", function(bullet){
+		console.log("new Bullet!");
+		console.log(bullets);
+		bullets.push(bullet);
+		// for(var x = bullets.length; x > 0; x--){
+		// 	if(Math.abs(bullets[x].bullet.x) > 2000 || Math.abs(bullets[x].y) > 2000){
+		// 		bullets[x].splice(x, 0);
+		// 	}
+		// }
+		console.log("sending bullets array");
+		socket.emit("bullets", {"bullets": bullets});
 	})
 })
