364a7b92b2be9fc2b55cbd22107733c3e275c4c9
Logan Sinclair <rocket3701@yahoo.com>
2017-09-22 23:20:12 -0500

    Add Bullet Collision Detection

--- app.js
+++ app.js
@@ -29,14 +29,66 @@ var canvasDimensions = {
 }
 
 var characterDimensions = {
-	height: 25,
-	width: 15
+	height: 15,
+	width: 25
 }
 
-//loop that sends out positions 60 times a second
+//loop that updates and sends out positions
 var loop = setInterval(function(){
-	io.sockets.emit("positions", {characters: characters, bullets: bullets});
+	updateCharacters();
+	updateBullets();
+	bulletCollsionDetection();
+	characterCollisionDetection();
+	emitArrays();
 }, 1000/60);
+
+function updateCharacters(){
+	for(var x = characters.length - 1; x >= 0; x--){
+		characters[x].x += characters[x].xIncr;
+		characters[x].y += characters[x].yIncr;
+	}
+}
+
+function updateBullets(){
+	for(var x = bullets.length - 1; x >= 0; x--){
+		bullets[x].x += bullets[x].xIncr;
+		bullets[x].y += bullets[x].yIncr;
+	}
+}
+
+function bulletCollisionDetection(){
+	for(var x = characters.length - 1; x >= 0; x--){
+		for(var i = bullets.length - 1; i >= 0; i--){
+			this.char = characters[x];
+			this.bull = bullets[i];
+			if(this.bull.x >= this.char.x && this.bull.x <= this.char.x + characterDimensions.width && this.bull.y >= this.char.y && this.bull.y <= this.char.y + characterDimensions.height){
+				this.char.health -= this.bull.damage;
+				if(this.char.health <= 1){
+					this.char.health = 10;
+					this.char.deaths += 1;
+					this.char.ammo = 10;
+					this.char.x = 0;
+					this.char.y = 0;
+					for(var y = characters.length - 1; y >= 0; y--){
+						if(characters[y].id === this.bull.owner){
+							characters[y].kills += 1;
+						}
+					}
+				}
+				bullets.splice(i, 1);
+			}
+		}
+	}
+}
+
+function characterCollisionDetection(){
+	//todo - damage players when in eachother
+}
+
+//emits positions to all clients
+function emitArrays(){
+	io.sockets.emit("positions", {characters: characters, bullets: bullets});
+};
 
 //socket.io connections
 io.on("connection", function(socket){
