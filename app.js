140cc2c0e61d6f56d7f8d2dd926291648d0a5123
Logan Sinclair <rocket3701@yahoo.com>
2017-09-22 22:55:25 -0500

    Deleted most of app.js, added socket.io receivers for bullets and chars
    Also added loop that emits the positions 60  times a second

--- app.js
+++ app.js
@@ -16,50 +16,33 @@ app.use(express.static('public'))
 var characters = [];
 var bullets = [];
 
+var canvasDimensions = {
+	height: 800,
+	width: 800
+}
+
+var characterDimensions = {
+	height: 25,
+	width: 15
+}
+
+//loop that runs 60 times a second
+var loop = setInterval(function(){
+	io.sockets.emit("positions", {characters: characters, bullets: bullets});
+}, 1000/60);
+
+//socket.io connections
 io.on("connection", function(socket){
 	socket.emit("connection", {successfully: "connected!"});
 	console.log("Someone has connected!");
+
+	//Receive Character
 	socket.on("character", function(data){
-		for(var i = 0; i < characters.length; i++){
-			if(characters[i].character.id === data.character.id){
-				characters.splice(i, 1);
-			}
-		}
 		characters.push(data);
-		socket.emit("serverCharacters", {data: characters});
-	})
-	socket.on("newBullet", function(bullet){
-		bullets.push(bullet);
-		// for(var x = bullets.length - 1; x >= 0; x--){
-		// 	if(Math.abs(bullets[x].bullet.x) > 2000 || Math.abs(bullets[x].y) > 2000){
-		// 		bullets[x].splice(x, 0);
-		// 	}
-		// }
-		io.sockets.emit("bullets", {"bullets": bullet});
-	})
-	socket.on("death", function(name){
+	});
 
-		var id = name.id;
-		var owner = name.owner;
-		io.sockets.emit("death", {name: id, owner: owner});
-		for(var x = characters.length - 1; x >= 0; x--){
-			var ch = characters[x];
-			if(ch.id === id){
-				characters.splice(x, 1);
-			}
-		}
+	//Receive Bullet
+	socket.on("bullet", function(data){
+		bullets.push(data);
 	})
-	socket.on("healthBubble", function(data){
-		io.sockets.emit("healthBubble", data);
-	});
-	socket.on("ammoBubble", function(data){
-		io.sockets.emit("ammoBubble", data);
-	});
-	socket.on(" speedBubble", function(data){
-		io.sockets.emit("speedBubble", data);
-	});
 })
-
-var characterRemoval = setInterval(function(){
-	characters = [];
-}, 60000)
